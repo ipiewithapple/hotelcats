edb9061afbd55e88b0a5bcdae16365238c153bd8
ipiewithapple <ipiewithapple@yandex.ru>
2020-02-18 15:38:15 +0300

    Mobile nav menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 // Menu burger
 const burger = document.querySelector('.nav__burger');
+const mobileMenu = document.querySelector('.nav-mobile');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('nav__burger--open');
+  mobileMenu.classList.toggle('nav-mobile--open');
 })
