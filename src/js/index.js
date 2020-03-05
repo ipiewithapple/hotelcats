359c3cbd0c7da12aff6778ed44688fa4744faa0f
ipiewithapple <ipiewithapple@yandex.ru>
2020-03-05 16:27:41 +0300

    Add wow.js and animation.css

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+import WOW from 'wow.js';
+
 // Menu burger
 const burger = document.querySelector('.nav__burger');
 const mobileMenu = document.querySelector('.nav-mobile');
@@ -6,3 +8,7 @@ burger.addEventListener('click', () => {
   burger.classList.toggle('nav__burger--open');
   mobileMenu.classList.toggle('nav-mobile--open');
 })
+
+// WOW init
+
+new WOW().init();
