d728a0247c66b469c91446fad3f8c683af11e7ee
ipiewithapple <ipiewithapple@yandex.ru>
2020-03-06 14:31:25 +0300

    Add animejs

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,17 @@
 import WOW from 'wow.js';
 import Menu from './menu.js';
+import Slider from './slider.js';
 
 // Menu burger
 Menu();
 
 // WOW init
 new WOW().init();
+
+// Slider
+const slider = new Slider('reviews', 'reviews__list', 'reviews__list-item');
+slider.onPointClick();
+const slider1 = new Slider('rooms', 'rooms__list', 'rooms__list-item');
+slider1.onPointClick();
+
 
---
+++ src/js/slider.js
@@ -0,0 +1,41 @@
+import anime from 'animejs/lib/anime.es.js';
+
+export default class Slider {
+  constructor(parent, slidesParent, slides) {
+    this.parent = parent;
+    this.slidesParent = document.querySelector(`.${slidesParent}`);
+    this.slides = document.querySelectorAll(`.${slides}`);
+    this.sliderPoints = document.querySelectorAll(`.${parent} .slider__points-item`);
+    this.sliderBtns = document.querySelectorAll(`.${parent} .slider__btn`);
+    this.sliderBtnLeft = document.querySelector(`.${parent} .slider__btn--left`);
+    this.sliderBtnRight = document.querySelector(`.${parent} .slider__btn--right`);
+  }
+
+  slidesAnim () {
+    
+  }
+
+  onPointClick() {
+    this.sliderPoints.forEach((point, i) => {
+      point.dataset.id = i;
+      point.addEventListener('click', () => {
+        this.sliderPoints.forEach((point) => {
+          point.classList.remove('slider__points-item--active');
+        });
+        point.classList.add('slider__points-item--active');
+        if (point.dataset.id == 0) {
+          anime({
+            targets: this.slidesParent,
+            translateX: 0,
+          });
+        } else {
+          anime({
+            targets: this.slidesParent,
+            translateX: -100,
+          });
+        }
+      })
+    });
+  }
+
+}
